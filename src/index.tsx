6c3513d801c136d187b393c5e62c3a7299a943c8
aric.zheng <aric.zheng@alo7.com>
2020-07-04 21:59:22 +0800

    feat: 原生 audio 实现

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App2';
 import * as serviceWorker from './serviceWorker';
 
 
